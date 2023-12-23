bc6056326ede1899496a7eab2b5a6c66fbcf8eb3
osantamaria <osantamaria@gmail.com>
2023-12-22 22:11:38 -0600

    section2

---
+++ task-manager-ui/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutComponent } from './components/about/about.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
