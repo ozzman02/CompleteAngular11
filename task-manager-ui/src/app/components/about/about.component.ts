8f32d94906758d94a59f93bcbc80f82587865bbd
osantamaria <osantamaria@gmail.com>
2023-12-23 17:19:02 -0600

    Section3

--- task-manager-ui/src/app/components/about/about.component.ts
+++ task-manager-ui/src/app/components/about/about.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
+  host: {ngSkipHydration: 'true'}
 })
 export class AboutComponent {
 
