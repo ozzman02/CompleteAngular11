2a40b86dc010a5df5969d248304c8d6f0c8a7063
osantamaria <osantamaria@gmail.com>
2023-12-24 18:25:27 -0600

    backend get request

--- task-manager-backend/src/main/java/com/taskmanager/controller/ProjectRestController.java
+++ task-manager-backend/src/main/java/com/taskmanager/controller/ProjectRestController.java
@@ -1,10 +1,8 @@
 package com.taskmanager.controller;
 
-import com.taskmanager.entity.Project;
 import com.taskmanager.model.ProjectDTO;
 import com.taskmanager.service.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
